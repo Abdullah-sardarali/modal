841cf94a5a7bcf247abd4fe02550656d790d353c
Abdullah-sardarali <iqraschoolumarzo@gmail.com>
2024-08-09 05:32:41 -0700

    Add files via upload

---
+++ New folder/about.js
@@ -0,0 +1,26 @@
+
+var btn = document.querySelector("button");
+
+btn.addEventListener("click",function(){
+    
+     document.querySelector("#fs").style.opacity= 1;
+     
+     document.querySelector("#model").style.top= "15%";
+
+})
+
+
+// var arry =[1,3,-45,-778,9,0,34,-5,8,7,-9,3,-6,89,0,6,-4,6,789,]
+
+// arry.forEach(function(val,index){
+//     if(val<0){
+//       arry.splice(index)
+//     }
+// })
+
+
+
+
+
+
+
